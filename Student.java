1c3223ed05df6d82214a82d4cff43056ca715be9
malhame3 <malhame3@asu.edu>
2024-10-09 10:11:22 +0500

    Documented Student.java

--- Student.java
+++ Student.java
@@ -5,10 +5,18 @@ import java.util.Map;
 
 public class Student extends User {
 
-    private Map<String, User> userDatabase = new HashMap<>();
+    private Map<String, User> userDatabase = new HashMap<>(); // Database to store user information
 
+    /**
+     * Constructor for the Student class.
+     *
+     * @param username The username for the student
+     * @param password The password for the student
+     * @param userDatabase The database of users
+     */
     public Student(String username, char[] password, Map<String, User> userDatabase) {
-        super(username, password);
-        this.userDatabase = userDatabase;
+        super(username, password); // Call the parent constructor to initialize username and password
+        this.userDatabase = userDatabase; // Initialize the user database
     }
+
 }
